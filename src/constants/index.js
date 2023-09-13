6c4dac89b9aa1268ada92eb169fe10426555fd7f
Dylan Petzer <dylan.petzer@entelect.co.za>
2023-09-13 10:04:22 +0200

    added navbar component and homepage, nothing on homepage as yet

---
+++ src/constants/index.js
@@ -0,0 +1,16 @@
+const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "services",
+      title: "Services",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+
+  export {navLinks};
