0ccffc129da84b200d0b44e1ce76284a00e9da76
DylanAbsa <dylan.petzer@absa.africa>
2023-09-13 13:30:35 +0200

    scroller

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -43,7 +43,7 @@ const Home = () => {
         <Lottie options={defaultOptions} height={animationSize.height}
                 width={animationSize.width}/>
         </div>
-      <div className="absolute xs:bottom-5 md:bottom-10 w-full flex justify-center items-center">
+      <div className="absolute xs:bottom-10 md:bottom-20 w-full flex justify-center items-center">
         <a href={`#about`}>
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-tertiary flex justify-center items-start p-2">
             <motion.div animate={{y: [0, 24, 0]}} transition={{duration: 1.5, repeat: Infinity, repeatType: 'loop' }} className="w-3 h-3 rounded-full bg-tertiary mb-1"/>
