1a46cd63d3d45f3e974e050993eaef1759cab97d
Dylan Petzer <dylan.petzer@entelect.co.za>
2023-09-13 21:02:04 +0200

    added footer with updated styles

--- src/constants/index.js
+++ src/constants/index.js
@@ -56,4 +56,63 @@ const navLinks = [
     },
   ];
 
-  export {navLinks, services};
+  const footerLinks = [
+    {
+        title: "About",
+        links: [{ label: "who we are", link: "" },
+        { label: "certificate", link: "" },
+        { label: "accreditation", link: "" },
+        { label: "contact", link: "" }
+    ]
+    },
+    {
+        title: "Declarations",
+        links: [ {
+            label: "Complaints, appeals and disputes",
+            link: "https://google.co.za/"
+        },
+        {
+            label: "Impartiality Statement",
+            link: "https://echo4x4rentals.co.za/"
+        },
+        {
+            label: "B-BBEE Consulting",
+            link: "https://echo4x4centre.co.za/"
+        },
+        {
+            label: "B-BBEE Certificate",
+            link: "https://echo4x4adventure.co.za/"
+        },
+        {
+          label: "Accreditation",
+          link: "https://echo4x4adventure.co.za/"
+        },
+        {
+          label: "Youth Employment Service",
+          link: "https://echo4x4adventure.co.za/"
+        },
+    ]
+    },
+    {
+        title: "Join Us",
+        links: [ {
+            label: "Discord",
+            link: "https://echo4x4.co.za/contact-us/"
+        },
+        {
+            label: "Facebook",
+            link: "https://www.facebook.com/Echo4x4/"
+        },
+        {
+            label: "Instagram",
+            link: "https://www.instagram.com/echo4x4/"
+        },
+        {
+            label: "Twitter",
+            link: "https://twitter.com/echo4x4"
+        }
+    ]
+    }
+];
+
+  export {navLinks, services, footerLinks};
