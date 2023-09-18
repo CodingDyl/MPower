b94e84a04676d15a5fa09353a6785f9436288c6f
DylanAbsa <dylan.petzer@absa.africa>
2023-09-18 10:51:18 +0200

    added office section info

--- src/components/Office.jsx
+++ src/components/Office.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import {SectionWrapper} from "../hoc/index.js";
 import {fadeIn, textVariant} from "../utils/motion.js";
+import {IconPhone, IconPrinter, IconMail, IconLocation} from "@tabler/icons-react";
 import {styles} from "../styles.js";
 import {motion} from "framer-motion";
+import {Typography} from "@mui/material";
 
 const Office = () => {
     return (
@@ -12,7 +14,7 @@ const Office = () => {
                 <h2 className={styles.sectionHeadText}>Head Office</h2>
             </motion.div>
 
-            <motion.p variants={fadeIn("", "", 0.1, 1)} className="mt-4 text-primary text-[17px] max-w-3xl leading-[30px]">
+            <motion.p variants={fadeIn("right", "tween", 0.1, 1)} className="mt-4 text-primary text-[17px] max-w-3xl leading-[30px]">
                 We're ready to lead you into the future of B-BBEE.
 
                 You're not going to hit a ridiculously long phone menu when you call us. Your email isn't going to the inbox abyss, never to be seen or heard from again. At mPowerRatings, we provide the exceptional service we'd want to experience ourselves!
@@ -21,6 +23,36 @@ const Office = () => {
 
                 At mPowerRatings, you always talk to a human!
             </motion.p>
+
+            <motion.div variants={fadeIn("left", "tween", 0.2, 1)} className="mt-4 text-primary text-[22px] md:grid md:grid-cols-2 md:gap-6 xs:flex xs:flex-col xs:justify-center xs:items-center">
+                <div className="flex gap-1 align-center items-center">
+                    <div className="flex justify-center items-center border-solid border-2 border-green-pink-gradient align-center rounded-full px-2 py-2 bg-white-100">
+                        <IconPhone />
+                    </div>
+                    <Typography>011- 880 0060/4</Typography>
+                </div>
+                <div className="flex gap-1 align-center items-center">
+                    <div className="flex justify-center items-center border-solid border-2 border-green-pink-gradient align-center rounded-full px-2 py-2 bg-white-100">
+                        <IconPrinter />
+                    </div>
+                    <Typography>086 666 4815</Typography>
+                </div>
+                <div className="flex gap-1 align-center items-center">
+                    <div className="flex justify-center items-center border-solid border-2 border-green-pink-gradient align-center rounded-full px-2 py-2 bg-white-100">
+                        <IconMail />
+                    </div>
+                    <Typography>info@mpowerratings.co.za</Typography>
+                </div>
+                <div className="flex gap-1 align-center items-center">
+                    <div className="flex justify-center items-center border-solid border-2 border-green-pink-gradient align-center rounded-full px-2 py-2 bg-white-100">
+                        <IconLocation />
+                    </div>
+                    <Typography>99 Jan Smuts Avenue,
+                        Saxonwold,
+                        2196,
+                        Johannesburg</Typography>
+                </div>
+            </motion.div>
         </>
     );
 }
