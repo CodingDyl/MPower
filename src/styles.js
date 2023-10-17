c2390205e774ff9cd82f6f66671b762650afab78
DylanAbsa <dylan.petzer@absa.africa>
2023-10-17 11:15:49 +0200

    updated title color on dark background

--- src/styles.js
+++ src/styles.js
@@ -11,7 +11,7 @@ const styles = {
     sectionHeadText:
       "text-primary font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
     sectionHeadTextContact:
-        "text-tertiary font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
+        "text-white-100 font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
     sectionSubText:
       "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
   };
